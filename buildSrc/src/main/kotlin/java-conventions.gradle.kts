6680bc9819e11777a6e442b1b50681522f4eae1c
Lucas Monteiro <lukasmonteir0@hotmail.com>
2025-03-22 22:12:44 -0300

    build: update dependencies version

--- buildSrc/src/main/kotlin/java-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/java-conventions.gradle.kts
@@ -12,8 +12,8 @@ repositories {
 dependencies {
     implementation("io.vavr:vavr-kotlin:${Version.VAVR}")
 
-    testImplementation("org.junit.jupiter:junit-jupiter:5.10.3")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.3")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.11.3")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.11.3")
 }
 
 tasks.withType<Test> {
